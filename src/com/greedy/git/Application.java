c6371b239c55c01a1074a061fcdcf4944b724ee1
jiwon <jiwon@DESKTOP-K28P42Q>
2021-07-22 11:29:20 +0900

    20210722_11_세번째 커밋 & 푸쉬

--- src/com/greedy/git/Application.java
+++ src/com/greedy/git/Application.java
@@ -3,10 +3,10 @@ package com.greedy.git;
 public class Application {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
 		
 		System.out.println("깃허브에 업로드 성공!");
+		
+		System.out.println("한줄 추가하기");
 	}
 
 }
